94e8e62871c6acd132c1e9e114a6cb9f2c05409f
mikdevio <mikdevio@gmail.com>
2026-04-17 22:55:43 -0500

    fea: Setup SidebarInset in MainLayout

--- api_todo_client/src/components/common/AppSideBar.tsx
+++ api_todo_client/src/components/common/AppSideBar.tsx
@@ -28,16 +28,16 @@ const data = {
 }
 
 
-const AppSideBar: React.FC<SideBarProps> = ({ sidebar_title }) => {
+function AppSideBar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
-        <Sidebar collapsible='offcanvas'>
+        <Sidebar collapsible='offcanvas' {...props}>
             <SidebarHeader>
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton asChild className='data-[slot=sidebar-menu-button]:p-1.5!'>
                             <a href='#'>
                                 <IconInnerShadowTop className='size-5!' />
-                                <span className='text-base font-semibold'>{sidebar_title}</span>
+                                <span className='text-base font-semibold'>API TODO</span>
                             </a>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
--- api_todo_client/src/components/layout/MainLayout.tsx
+++ api_todo_client/src/components/layout/MainLayout.tsx
@@ -1,18 +1,29 @@
 import { Outlet } from "react-router-dom";
-import { SidebarProvider } from "../ui";
-import AppSideBar from "../common/SideBar";
+import { SidebarInset, SidebarProvider } from "@/components/ui";
+import AppSideBar from "@/components/common/AppSideBar";
+import AppHeader from "@/components/common/AppHeader";
 
 const MainLayout = () => {
     return (
-        <SidebarProvider>
-            <AppSideBar sidebar_title="API-TODO" />
-            <div className='flex min-h-screen'>
-                <div className="flex-1">
-                    <main className="p-6">
-                        <Outlet />
-                    </main>
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSideBar variant="inset" />
+            <SidebarInset>
+                <AppHeader />
+                <div className='flex min-h-screen'>
+                    <div className="flex-1">
+                        <main className="p-6">
+                            <Outlet />
+                        </main>
+                    </div>
                 </div>
-            </div>
+            </SidebarInset>
         </SidebarProvider>
     )
 }
